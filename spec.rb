a253320c03a66e99a68eb3715f386305d3afffb5
James Lett <jlett89@gmail.com>
2016-01-05 14:55:02 -0800

    add some error tests all 8 tests are failing

--- spec.rb
+++ spec.rb
@@ -19,4 +19,24 @@ describe "FuzzBuzz" do
   it "returns FuzzBuzz for multiples fo 15" do
     @fb.print(15*@number).must_equal "FuzzBuzz"
   end
+
+  it "returns the number as a string for all non FuzzBuzz numbers" do
+    @fb.print(@number).must_equal @number.to_s
+  end
+
+  it "returns an Array object when a range is given as an argument" do
+    @fb.print(1..5).must_be_instance_of Array
+  end
+
+  it "raises an error if the number is not an negative" do
+    proc {@fb.print(-2)}.must_raise Argument Error
+  end
+
+  it "raises an error if the number is not an zero" do
+    proc {@fb.print(0)}.must_raise Argument Error
+  end
+
+  it "raises an error if the number is not an Integer" do
+    proc {@fb.print(2.5)}.must_raise Argument Error
+  end
 end
